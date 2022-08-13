2286b04cd80622ee1b5d4752e7f295a8b16607bf
sai7721 <sai7721@gmail.com>
2022-08-13 23:30:19 +0900

    Update scalpingAutoTradeWithSlack.py

--- scalpingAutoTradeWithSlack.py
+++ scalpingAutoTradeWithSlack.py
@@ -1,7 +1,9 @@
 import time
+from numpy import true_divide
 import pyupbit
 import datetime
 import requests
+import math
 
 access = "SijZbF3zKypf6A9SBbEgg8XxuUgR8YxmxG0P0OtP"
 secret = "K6OKreFQ1GPGmzAx6p1VGdU4Ac2keR1eIlM9DPtd"
@@ -49,9 +51,10 @@ def get_current_price(ticker):
 
 # 로그인
 upbit = pyupbit.Upbit(access, secret)
-print("autotrade start")
 # 시작 메세지 슬랙 전송
 post_message(myToken,"#coin", "autotrade start")
+
+buyCooldown = True
 
 while True:
     try:
@@ -60,22 +63,19 @@ while True:
         end_time = start_time + datetime.timedelta(days=1)
         # 9:00 < 현재 < 8:59:50
         if start_time < now < end_time - datetime.timedelta(seconds=10):
-            target_price = get_target_price("KRW-SAND", 0.6)
+            target_price = get_target_price("KRW-SAND", 0.3)
             ma15 = get_ma15("KRW-SAND")
             current_price = get_current_price("KRW-SAND")
-            if target_price < current_price and ma15 < current_price:
-                krw = get_balance("KRW")
-                # 최소 거래 금액이 5천원이기 때문에, 해당 금액 이상 있을 경우 매수한다.
-                if krw > 5000:
-                    buy_result = upbit.buy_market_order("KRW-SAND", krw*0.9995) # 수수료 금액을 제외한 금액 만큼 매수한다.
-                    post_message(myToken,"#coin", "SAND buy : " +str(buy_result))
+            if target_price < current_price and ma15 < current_price and buyCooldown:
+                buy_result = upbit.buy_market_order("KRW-SAND", math.floor(get_balance("KRW")*0.9995)) # 수수료 금액을 제외한 금액 만큼 매수한다.
+                post_message(myToken,"#coin", "SAND buy : " +str(buy_result))
+                buyCooldown = False
         else:
-            btc = get_balance("SAND")
-            # 코인 매도 시에도 최소 금액이 필요하다. 그런데 대부분 들고있는 갯수가 5천원 이상일테니.. 그냥 대충 넣어주자
-            if btc > 0.005:
-                sell_result = upbit.sell_market_order("KRW-SAND", btc)
-                # sell_result = upbit.sell_market_order("KRW-SAND", btc*0.9995) # 팔때는 수수료를 감안하지 않아도 된다.
-                post_message(myToken,"#coin", "SAND sell : " +str(sell_result))
+            buyCooldown = True
+            sell_result = upbit.sell_market_order("KRW-SAND") #2번째 인자값이 없으면 전량 매도 한다고한다..
+            # sell_result = upbit.sell_market_order("KRW-SAND", get_balance("SAND"))
+            # sell_result = upbit.sell_market_order("KRW-SAND", get_balance("SAND")*0.9995) # 팔때는 수수료를 감안하지 않아도 된다.
+            post_message(myToken,"#coin", "SAND sell : " +str(sell_result))
         time.sleep(1)
     except Exception as e:
         print(e)
